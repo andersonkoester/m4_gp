5959dccfcd8d1f0c34409556abe16edd56a99fff
Anderson Koester <anderson@koester.com.br>
2022-06-04 07:29:44 -0300

    cronograma

--- src/app/pages/cronograma/cronograma.component.ts
+++ src/app/pages/cronograma/cronograma.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import dxGantt from 'devextreme/ui/gantt';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import ArrayStore from 'devextreme/data/array_store';
 
 export class Task {
   id: number;
@@ -13,142 +13,142 @@ export class Task {
 
 @Component({
   templateUrl: 'cronograma.component.html',
-  styleUrls: [ './cronograma.component.scss' ]
+  styleUrls: ['./cronograma.component.scss'],
 })
 export class CronogramaComponent implements OnInit {
+  tasksDataSource: ArrayStore;
 
-  tasks : Task[];
-  resources : any[];
+  tasks: Task[];
+  resources: any[];
 
   constructor() {
     this.tasks = [
       {
         id: 1,
         parentId: -1,
-        title: 'E-Commerce',
-        start: new Date('05/30/2022'),
-        end: new Date('06/04/2022'),
-        progress: 0,
-        taskColor: 'red'
-      },
+        title: 'E-Commerce'
+      } as Task,
       {
         id: 2,
         parentId: 1,
-        title: 'Gestão do Projeto',
-        start: new Date('05/30/2022'),
-        end: new Date('06/04/2022'),
-        progress: 0,
-        taskColor: 'red'
-      },
+        title: 'Gestão do Projeto'
+      } as Task,
       {
         id: 3,
-        parentId: 1,
-        title: 'Requisitos',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 2,
+        title: 'Gestão do Projeto',
+        start: new Date('05/30/2022'),
+        end: new Date('06/15/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 4,
-        parentId: 3,
-        title: 'Levantamento de Requisitos',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
-        progress: 0,
-        taskColor: 'red'
-      },
+        parentId: 1,
+        title: 'Requisitos'
+      } as Task,
       {
         id: 5,
-        parentId: 3,
-        title: 'Análise de Requisitos',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 4,
+        title: 'Levantamento de Requisitos',
+        start: new Date('06/10/2022'),
+        end: new Date('06/16/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 6,
-        parentId: 3,
-        title: 'Documentação de Requisitos',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 4,
+        title: 'Análise de Requisitos',
+        start: new Date('06/15/2022'),
+        end: new Date('06/21/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 7,
-        parentId: 1,
-        title: 'Desenvolvimento com Testes',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 4,
+        title: 'Documentação de Requisitos',
+        start: new Date('06/20/2022'),
+        end: new Date('06/24/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 8,
-        parentId: 7,
-        title: 'Desenvolvimento dos módulos',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
-        progress: 0,
-        taskColor: 'red'
-      },
+        parentId: 1,
+        title: 'Desenvolvimento com Testes'
+      } as Task,
       {
         id: 9,
-        parentId: 7,
-        title: 'Testes',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 8,
+        title: 'Desenvolvimento dos módulos',
+        start: new Date('06/25/2022'),
+        end: new Date('07/25/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 10,
-        parentId: 1,
-        title: 'Correção de Erros',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 8,
+        title: 'Testes',
+        start: new Date('07/20/2022'),
+        end: new Date('07/31/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 11,
         parentId: 1,
-        title: 'Planejamento de Sprints',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
-        progress: 0,
-        taskColor: 'red'
-      },
+        title: 'Correção de Erros'
+      } as Task,
       {
         id: 12,
         parentId: 11,
-        title: 'Módulo de Administração',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        title: 'Correção de Erros',
+        start: new Date('07/31/2022'),
+        end: new Date('08/05/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
       },
       {
         id: 13,
-        parentId: 11,
-        title: 'Módulo de Produtos',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
-        progress: 0,
-        taskColor: 'red'
-      },
+        parentId: 1,
+        title: 'Planejamento de Sprints'
+      } as Task,
       {
         id: 14,
-        parentId: 11,
-        title: 'Módulo de Compras',
-        start: new Date('2022-05-30'),
-        end: new Date('2022-06-04'),
+        parentId: 13,
+        title: 'Módulo de Administração',
+        start: new Date('06/25/2022'),
+        end: new Date('06/27/2022'),
         progress: 0,
-        taskColor: 'red'
+        taskColor: '#1db2f5',
+      },
+      {
+        id: 15,
+        parentId: 13,
+        title: 'Módulo de Produtos',
+        start: new Date('06/27/2022'),
+        end: new Date('06/29/2022'),
+        progress: 0,
+        taskColor: '#1db2f5',
+      },
+      {
+        id: 16,
+        parentId: 13,
+        title: 'Módulo de Compras',
+        start: new Date('06/29/2022'),
+        end: new Date('07/01/2022'),
+        progress: 0,
+        taskColor: '#1db2f5',
       },
     ];
+
+    this.tasksDataSource = new ArrayStore({
+      key: 'ID',
+      data: this.tasks,
+    });
 
     this.resources = [
       {
@@ -157,27 +157,118 @@ export class CronogramaComponent implements OnInit {
       },
       {
         id: 2,
-        text: "Analista"
+        text: 'Analista',
       },
       {
         id: 3,
-        text: "Dev. 1"
+        text: 'Dev. 1',
       },
       {
         id: 4,
-        text: "Dev. 2"
-      }
+        text: 'Dev. 2',
+      },
     ];
   }
 
   ngOnInit(): void {}
 
-  resourceAssignments = [{
-    id: 1,
-    taskId: 1,
-    resourceId: 1,
-  }];
+  resourceAssignments = [
+    {
+      id: 1,
+      taskId: 3,
+      resourceId: 1,
+    },
+    {
+      id: 3,
+      taskId: 5,
+      resourceId: 2,
+    },
+    {
+      id: 4,
+      taskId: 6,
+      resourceId: 2,
+    },
+    {
+      id: 5,
+      taskId: 7,
+      resourceId: 2,
+    },
+    {
+      id: 7,
+      taskId: 9,
+      resourceId: 2,
+    },
+    {
+      id: 8,
+      taskId: 9,
+      resourceId: 3,
+    },
+    {
+      id: 9,
+      taskId: 10,
+      resourceId: 3,
+    },
+    {
+      id: 10,
+      taskId: 10,
+      resourceId: 4,
+    },
+    {
+      id: 11,
+      taskId: 12,
+      resourceId: 3,
+    },
+    {
+      id: 12,
+      taskId: 12,
+      resourceId: 4,
+    },
+    {
+      id: 13,
+      taskId: 14,
+      resourceId: 2,
+    },
+    {
+      id: 14,
+      taskId: 15,
+      resourceId: 2,
+    },
+    {
+      id: 15,
+      taskId: 16,
+      resourceId: 2,
+    },
+    {
+      id: 16,
+      taskId: 14,
+      resourceId: 3,
+    },
+    {
+      id: 17,
+      taskId: 15,
+      resourceId: 3,
+    },
+    {
+      id: 18,
+      taskId: 16,
+      resourceId: 3,
+    },
+    {
+      id: 19,
+      taskId: 14,
+      resourceId: 4,
+    },
+    {
+      id: 20,
+      taskId: 15,
+      resourceId: 4,
+    },
+    {
+      id: 21,
+      taskId: 16,
+      resourceId: 4,
+    }
+  ];
 
   dependencies = [];
-
 }
